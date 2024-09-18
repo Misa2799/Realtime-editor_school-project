f7d551cd47add199c9c96d06ef4a4c619d75deaf
VictorSRV <155479482+VictorSRV@users.noreply.github.com>
2024-09-18 09:48:16 -0700

    add quill in docdetail

--- frontend/src/pages/DocumentDetail.tsx
+++ frontend/src/pages/DocumentDetail.tsx
@@ -1,11 +1,21 @@
 import { useParams } from "react-router-dom";
+import { useEffect} from "react";
+import Quill from "quill";
+import "quill/dist/quill.snow.css"
 
 export const DocumentDetail = () => {
-    const { id } = useParams();
+    const { id } = useParams(); 
+
+    useEffect(() => {
+        new Quill("#container", {theme: "snow"})
+    }, [])
 
     return (
         <div>
             <h1>Document Detail: {id}</h1>
+            Text Editor
+            <div id="container"></div>
         </div>
     );
 }
+
--- frontend/src/pages/TextEditor.tsx
+++ frontend/src/pages/TextEditor.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+
 
 export default function TextEditor() {
   return (
     <div>
-      
+      Text Editor
     </div>
   )
 }
