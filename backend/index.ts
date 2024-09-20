5a4c59add27b74f2d0de9053c499430816858ad0
VictorSRV <155479482+VictorSRV@users.noreply.github.com>
2024-09-19 19:05:38 -0700

    just add comment

--- backend/index.ts
+++ backend/index.ts
@@ -5,6 +5,8 @@ app.listen(PORT, () => {
     console.log(`[server]: listening at http://localhost:${PORT} in ${ENV} mode`);
 });
 
+
+//for run server socket.io
 server.listen(3001, () => {
     console.log(`[socket]: listening at http://localhost:3001 in ${ENV} mode`);
 });
