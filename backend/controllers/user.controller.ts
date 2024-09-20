4c01f783e83d224689096420b24a23474308025d
mika.urakawa <awakaru0122@gmail.com>
2024-09-20 11:33:41 -0700

    feat: create user api

--- backend/controllers/user.controller.ts
+++ backend/controllers/user.controller.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
-import { get } from "../models/user";
+import { get as getFromModel } from "../models/user";
 
 export const getUser = async (req: Request, res: Response) => {
     const { email } = req.body;
 
-    const user = await get(email);
+    const user = await getFromModel(email);
 
     if (!user) {
         res.status(404).send("User not found");
